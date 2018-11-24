f042184c9874def34444fda248e5aec001c4e988
Sean DuBois <sean@siobud.com>
2018-11-24 01:00:01 -0800

    Pass ICE creds directly to WithICECredentials
    Instead of allocating a temporary variable, no real change. Just a tiny
    bit less state to worry about.
    
    Resolves #204

--- rtcpeerconnection.go
+++ rtcpeerconnection.go
@@ -1151,11 +1151,10 @@ func (pc *RTCPeerConnection) addRTPMediaSection(d *sdp.SessionDescription, codec
 	if codecs := pc.mediaEngine.getCodecsByKind(codecType); len(codecs) == 0 {
 		return false
 	}
-	localUfrag, localPwd := pc.networkManager.IceAgent.GetLocalUserCredentials()
 	media := sdp.NewJSEPMediaDescription(codecType.String(), []string{}).
 		WithValueAttribute(sdp.AttrKeyConnectionSetup, dtlsRole.String()). // TODO: Support other connection types
 		WithValueAttribute(sdp.AttrKeyMID, midValue).
-		WithICECredentials(localUfrag, localPwd).
+		WithICECredentials(pc.networkManager.IceAgent.GetLocalUserCredentials()).
 		WithPropertyAttribute(sdp.AttrKeyRtcpMux).  // TODO: support RTCP fallback
 		WithPropertyAttribute(sdp.AttrKeyRtcpRsize) // TODO: Support Reduced-Size RTCP?
 
@@ -1185,8 +1184,6 @@ func (pc *RTCPeerConnection) addRTPMediaSection(d *sdp.SessionDescription, codec
 }
 
 func (pc *RTCPeerConnection) addDataMediaSection(d *sdp.SessionDescription, midValue string, candidates []string, dtlsRole sdp.ConnectionRole) {
-	localUfrag, localPwd := pc.networkManager.IceAgent.GetLocalUserCredentials()
-
 	media := (&sdp.MediaDescription{
 		MediaName: sdp.MediaName{
 			Media:   "application",
@@ -1206,7 +1203,7 @@ func (pc *RTCPeerConnection) addDataMediaSection(d *sdp.SessionDescription, midV
 		WithValueAttribute(sdp.AttrKeyMID, midValue).
 		WithPropertyAttribute(RTCRtpTransceiverDirectionSendrecv.String()).
 		WithPropertyAttribute("sctpmap:5000 webrtc-datachannel 1024").
-		WithICECredentials(localUfrag, localPwd)
+		WithICECredentials(pc.networkManager.IceAgent.GetLocalUserCredentials())
 
 	for _, c := range candidates {
 		media.WithCandidate(c)
